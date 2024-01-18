3006ac83263ce9e0e184500f2363af7eb4cdb945
pablo costa <126683635+pablomendoncacosta@users.noreply.github.com>
2024-01-18 09:53:56 -0400

    implementaçao de outra paginas tabela

---
+++ conexao.php
@@ -0,0 +1,11 @@
+<?php
+    $dbhost="localhost";
+    $dbusername="root";
+    $dbpassaword="";
+    $dbname="fazenda_mongarosa";
+    $conexao=new mysqli($dbhost,$dbusername,$dbpassaword,$dbname);
+    $conexao -> set_charset("UTF8");
+
+
+
+?>
